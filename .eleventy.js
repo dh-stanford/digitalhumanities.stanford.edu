2df418af6dd718b292c0e1fa99552d6cd90b7595
Simon Wiles <simonjwiles@gmail.com>
2022-06-15 10:49:45 -0700

    Add a paginated list of posts to the home page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addCollection("pages", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("./src/*.md")
+      .sort(
+        (a, b) => new Date(b.data.submitted_at) - new Date(a.data.submitted_at),
+      );
+  });
+
   return {
     dir: {
       input: "src",
