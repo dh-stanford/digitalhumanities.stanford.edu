ad5d541a7da3b120eed0bd617cf17cf816fcf34d
Simon Wiles <simonjwiles@gmail.com>
2022-06-30 21:29:25 -0700

    Add automatic excerpt generatio

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const CleanCSS = require("clean-css");
 const markdownIt = require("markdown-it");
+const striptags = require("striptags");
 
 const md = markdownIt();
 
@@ -19,6 +20,8 @@ module.exports = function (eleventyConfig) {
     return new CleanCSS({}).minify(code).styles;
   });
 
+  eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
+
   eleventyConfig.addPairedShortcode(
     "markdown",
     (markdownString, inline = null) =>
@@ -35,3 +38,22 @@ module.exports = function (eleventyConfig) {
     markdownTemplateEngine: "njk",
   };
 };
+
+function extractExcerpt(article) {
+  if (!article.hasOwnProperty("templateContent")) {
+    console.warn(
+      'Failed to extract excerpt: Document has no property "templateContent".',
+    );
+    return null;
+  }
+
+  let excerpt = null;
+  const content = article.templateContent;
+
+  excerpt = striptags(content)
+    .substring(0, 200) // Cap at 200 characters
+    .replace(/^\s+|\s+$|\s+(?=\s)/g, "")
+    .trim()
+    .concat("...");
+  return excerpt;
+}
