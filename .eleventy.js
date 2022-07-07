9cdcc5572eb733000d53e8ac7ebc92f3ec02a21f
Simon Wiles <simonjwiles@gmail.com>
2022-07-07 14:17:40 -0700

    Add a `dateFormat` filter
    This is a workaround for inconsistent date handling in 11ty/YAML/liquid/nunjucks.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const CleanCSS = require("clean-css");
+const { DateTime } = require("luxon");
 
 const { renderMarkdown, extractExcerpt } = require("./lib/utils");
 
@@ -22,6 +23,17 @@ module.exports = function (eleventyConfig) {
     "cssmin",
     (code) => new CleanCSS({}).minify(code).styles,
   );
+
+  eleventyConfig.addFilter("formatDate", (dateObj) => {
+    // 11ty and the YAML parser default to converting dates to UTC (or
+    //  assuming UTC if the TZ isn't specified) which affects all input dates.
+    // However, all output dates are assumed to be in the local timezone...
+    // (There is some hand-waving about it here:
+    //  https://www.11ty.dev/docs/dates/#dates-off-by-one-day)
+    return DateTime.fromJSDate(dateObj, { zone: "UTC" }).toLocaleString(
+      DateTime.DATE_FULL,
+    );
+  });
 
   eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
 
