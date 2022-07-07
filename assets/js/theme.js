a01287fa2331785357bafbe95be8ef6dcfde4b91
Simon Wiles <simonjwiles@gmail.com>
2022-07-06 22:43:49 -0700

    Rewrite "Hide Header on on scroll down" but better and without jQuery

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,46 +1,28 @@
-
 // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('nav').outerHeight();
+const navbar = document.querySelector(".topnav");
+const navbarHeight = navbar.getBoundingClientRect().height;
+let lastKnownScrollPosition = 0;
+let ticking = false;
 
-    $(window).scroll(function(event){
-        didScroll = true;
+const onScroll = (scrollPos) => {
+  if (scrollPos > lastKnownScrollPosition && scrollPos > navbarHeight) {
+    navbar.classList.remove("nav-down");
+    navbar.classList.add("nav-up");
+    navbar.style.top = `-${navbarHeight}px`;
+  } else {
+    navbar.classList.add("nav-down");
+    navbar.classList.remove("nav-up");
+    navbar.style.top = "0px";
+  }
+};
+
+document.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      onScroll(window.scrollY);
+      ticking = false;
+      lastKnownScrollPosition = window.scrollY;
     });
-
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down            
-            $('nav').removeClass('nav-down').addClass('nav-up'); 
-            $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-           
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {               
-                $('nav').removeClass('nav-up').addClass('nav-down');
-                $('.nav-up, .nav-down').css('top', '0px');             
-            }
-        }
-
-        lastScrollTop = st;
-    }
-    
-    
-    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
-
+    ticking = true;
+  }
+});
